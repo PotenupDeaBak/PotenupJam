cc0de7f2c0e8698771b8727105daa0ca326aa169
jun <50354282+wjdwns@users.noreply.github.com>
2026-04-13 18:10:14 +0900

    add socket module, socket listen, change fov

--- Source/PotenupJam/PotenupJam.Build.cs
+++ Source/PotenupJam/PotenupJam.Build.cs
@@ -8,7 +8,7 @@ public class PotenupJam : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "WebSockets", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
